6cab41ad503ec63e55a9d5f581e491cd1543ddfa
yakjuly <yakjuly@gmail.com>
2015-10-11 09:48:08 +0800

    format hash operation

--- spec/support/data.rb
+++ spec/support/data.rb
@@ -14,10 +14,10 @@ post1 = Post.create(title: "First post!")
 post2 = Post.create(title: "Second post!")
 post3 = Post.create(title: "Third post!")
 
-post1.comments.create(status: 'created', rating: 4, :author => student1)
-post1.comments.create(status: 'approved', rating: 5)
-post1.comments.create(status: 'deleted', rating: 0, :author => student2)
+post1.comments.create(status: 'created', rating: 4, author: student1)
+post1.comments.create(status: 'approved', rating: 5, author: student1)
+post1.comments.create(status: 'deleted', rating: 0, author: student2)
 
-post2.comments.create(status: 'approved', rating: 3, :author => student1)
+post2.comments.create(status: 'approved', rating: 3, author: student1)
 post2.comments.create(status: 'approved', rating: 5)
 
