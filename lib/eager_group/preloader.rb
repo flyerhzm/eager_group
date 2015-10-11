a35d6bb8dbb63ea9b8cf687ae345fd32827281d9
yakjuly <yakjuly@gmail.com>
2015-10-11 10:19:40 +0800

    use lambda

--- lib/eager_group/preloader.rb
+++ lib/eager_group/preloader.rb
@@ -16,21 +16,24 @@ module EagerGroup
           reflection = @klass.reflect_on_association(definition.association)
           association_class = reflection.class_name.constantize
           association_class = association_class.instance_exec(&definition.scope) if definition.scope
-          if reflection.through_reflection
-            foreign_key = "#{reflection.through_reflection.name}.#{reflection.through_reflection.foreign_key}"
-            as_where_condition = if reflection.through_reflection.options[:as]
-              # After Rails 4.0. STI use base_class as polymorphic type
-              ["#{reflection.through_reflection.options[:as]}_type = ?", @klass.base_class.name]
+          through_as_condition = lambda {|reflection|
+            if reflection.type
+              ["#{reflection.name}.#{reflection.type} = ?", @klass.base_class.name]
             else
               []
             end
+          }
+          
+          if reflection.through_reflection
+            foreign_key = "#{reflection.through_reflection.name}.#{reflection.through_reflection.foreign_key}"
             aggregate_hash = association_class.joins(reflection.through_reflection.name)
                                               .where("#{foreign_key} IN (?)", record_ids)
-                                              .where(as_where_condition)
+                                              .where(through_as_condition.call(reflection.through_reflection))
                                               .group("#{foreign_key}")
                                               .send(definition.aggregate_function, definition.column_name)
           else
             aggregate_hash = association_class.where(reflection.foreign_key => record_ids)
+                                              .where(through_as_condition.call(reflection))
                                               .group(reflection.foreign_key)
                                               .send(definition.aggregate_function, definition.column_name)
           end
@@ -41,5 +44,6 @@ module EagerGroup
         end
       end
     end
+    
   end
 end
