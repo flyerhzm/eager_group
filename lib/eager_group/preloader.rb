c1520334a1eebd8e9bd751dc2b88ec1ab0c1bc16
Awesome Code <team@xinminlabs.com>
2018-12-15 08:12:24 +0000

    Auto corrected by following Ruby Style/Next

--- lib/eager_group/preloader.rb
+++ lib/eager_group/preloader.rb
@@ -19,28 +19,27 @@ module EagerGroup
           @klass = @records.first.class
         end
         record_ids = @records.map { |record| record.send(primary_key) }
-        if definition = @klass.eager_group_definitions[definition_key]
-          reflection = @klass.reflect_on_association(definition.association)
-          association_class = reflection.klass
-          association_class = association_class.instance_exec(*arguments, &definition.scope) if definition.scope
+        next unless definition = @klass.eager_group_definitions[definition_key]
+        reflection = @klass.reflect_on_association(definition.association)
+        association_class = reflection.klass
+        association_class = association_class.instance_exec(*arguments, &definition.scope) if definition.scope
 
-          if reflection.through_reflection
-            foreign_key = "#{reflection.through_reflection.name}.#{reflection.through_reflection.foreign_key}"
-            aggregate_hash = association_class.joins(reflection.through_reflection.name)
-                                              .where(foreign_key => record_ids)
-                                              .where(polymophic_as_condition(reflection.through_reflection))
-                                              .group(foreign_key)
-                                              .send(definition.aggregate_function, definition.column_name)
-          else
-            aggregate_hash = association_class.where(reflection.foreign_key => record_ids)
-                                              .where(polymophic_as_condition(reflection))
-                                              .group(reflection.foreign_key)
-                                              .send(definition.aggregate_function, definition.column_name)
-          end
-          @records.each do |record|
-            id = record.send(primary_key)
-            record.send("#{definition_key}=", aggregate_hash[id] || 0)
-          end
+        if reflection.through_reflection
+          foreign_key = "#{reflection.through_reflection.name}.#{reflection.through_reflection.foreign_key}"
+          aggregate_hash = association_class.joins(reflection.through_reflection.name)
+                                            .where(foreign_key => record_ids)
+                                            .where(polymophic_as_condition(reflection.through_reflection))
+                                            .group(foreign_key)
+                                            .send(definition.aggregate_function, definition.column_name)
+        else
+          aggregate_hash = association_class.where(reflection.foreign_key => record_ids)
+                                            .where(polymophic_as_condition(reflection))
+                                            .group(reflection.foreign_key)
+                                            .send(definition.aggregate_function, definition.column_name)
+        end
+        @records.each do |record|
+          id = record.send(primary_key)
+          record.send("#{definition_key}=", aggregate_hash[id] || 0)
         end
       end
     end
