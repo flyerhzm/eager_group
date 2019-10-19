b845e74ca18ca524974189b4e0b287636715471c
Richard Huang <flyerhzm@gmail.com>
2019-10-19 22:39:13 +0800

    remove unused all scope

--- spec/integration/eager_group_spec.rb
+++ spec/integration/eager_group_spec.rb
@@ -6,21 +6,21 @@ RSpec.describe EagerGroup, type: :model do
   describe '.eager_group' do
     context 'has_many' do
       it 'gets Post#approved_comments_count' do
-        posts = Post.all.eager_group(:approved_comments_count)
+        posts = Post.eager_group(:approved_comments_count)
         expect(posts[0].approved_comments_count).to eq 1
         expect(posts[1].approved_comments_count).to eq 2
         expect(posts[2].approved_comments_count).to eq 0
       end
 
       it 'gets Post#comments_average_rating' do
-        posts = Post.all.eager_group(:comments_average_rating)
+        posts = Post.eager_group(:comments_average_rating)
         expect(posts[0].comments_average_rating).to eq 3
         expect(posts[1].comments_average_rating).to eq 4
         expect(posts[2].comments_average_rating).to eq 0
       end
 
       it 'gets both Post#approved_comments_count and Post#comments_average_rating' do
-        posts = Post.all.eager_group(:approved_comments_count, :comments_average_rating)
+        posts = Post.eager_group(:approved_comments_count, :comments_average_rating)
         expect(posts[0].approved_comments_count).to eq 1
         expect(posts[0].comments_average_rating).to eq 3
         expect(posts[1].approved_comments_count).to eq 2
@@ -31,7 +31,7 @@ RSpec.describe EagerGroup, type: :model do
 
       it 'gets Post#comments_average_rating_by_author' do
         students = Student.all
-        posts = Post.all.eager_group([:comments_average_rating_by_author, students[0], true])
+        posts = Post.eager_group([:comments_average_rating_by_author, students[0], true])
         expect(posts[0].comments_average_rating_by_author).to eq 4.5
         expect(posts[1].comments_average_rating_by_author).to eq 3
       end
@@ -49,7 +49,7 @@ RSpec.describe EagerGroup, type: :model do
       end
 
       it 'gets Post#first_comment and Post#last_comment' do
-        posts = Post.all.eager_group(:first_comment, :last_comment)
+        posts = Post.eager_group(:first_comment, :last_comment)
         expect(posts[0].first_comment).to eq posts[0].comments.first
         expect(posts[1].first_comment).to eq posts[1].comments.first
         expect(posts[2].first_comment).to be_nil
@@ -73,7 +73,7 @@ RSpec.describe EagerGroup, type: :model do
 
     context 'has_many :through' do
       it 'gets Teacher#students_count' do
-        teachers = Teacher.all.eager_group(:students_count)
+        teachers = Teacher.eager_group(:students_count)
         expect(teachers[0].students_count).to eq 1
         expect(teachers[1].students_count).to eq 3
         expect(teachers[2].students_count).to eq 0
@@ -82,7 +82,7 @@ RSpec.describe EagerGroup, type: :model do
 
     context 'has_many :as, has_many :through' do
       it 'gets Student#posts_count' do
-        students = Student.all.eager_group(:posts_count)
+        students = Student.eager_group(:posts_count)
         expect(students[0].posts_count).to eq 2
         expect(students[1].posts_count).to eq 1
         expect(students[2].posts_count).to eq 0
@@ -93,7 +93,7 @@ RSpec.describe EagerGroup, type: :model do
   describe '.preload_eager_group' do
     context 'Cache query result' do
       it 'eager_group result cached' do
-        posts = Post.all.eager_group(:approved_comments_count)
+        posts = Post.eager_group(:approved_comments_count)
         post = posts[0]
         object_id1 = post.instance_variable_get('@approved_comments_count').object_id
         object_id2 = post.approved_comments_count.object_id
@@ -104,7 +104,7 @@ RSpec.describe EagerGroup, type: :model do
 
       it 'eager_group result cached if arguments given' do
         students = Student.all
-        posts = Post.all.eager_group([:comments_average_rating_by_author, students[0], true])
+        posts = Post.eager_group([:comments_average_rating_by_author, students[0], true])
         post = posts[0]
         object_id1 = post.instance_variable_get('@comments_average_rating_by_author').object_id
         object_id2 = post.comments_average_rating_by_author.object_id
