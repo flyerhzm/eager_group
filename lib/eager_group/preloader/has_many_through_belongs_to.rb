d9f41bd08010e3a78e130b86f20395f241206f23
Richard Huang <flyerhzm@gmail.com>
2022-12-04 13:25:38 +0800

    add frozen_string_literal: true

--- lib/eager_group/preloader/has_many_through_belongs_to.rb
+++ lib/eager_group/preloader/has_many_through_belongs_to.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module EagerGroup
   class Preloader
     class HasManyThroughBelongsTo < AggregationFinder
--- lib/eager_group/preloader/many_to_many.rb
+++ lib/eager_group/preloader/many_to_many.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module EagerGroup
   class Preloader
     class ManyToMany < AggregationFinder
